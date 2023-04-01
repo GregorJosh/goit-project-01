82c56c1ce014adb42c29811fe28fa237faa16c46
GregorJosh <33376735+GregorJosh@users.noreply.github.com>
2023-04-01 18:55:50 +0000

    Deploying to gh-pages from @ GregorJosh/goit-project-01@09db4624d885110230053161ebb8803dfd4afefc 🚀

---
+++ index.7ed27d03.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[pop-up-open]"),closeModalBtn:document.querySelector("[pop-up-close]"),modal:document.querySelector("[pop-up]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.7ed27d03.js.map
