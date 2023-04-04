b61fed84c21bc235f06f1e42100d7d717174d649
Domin737 <p.dominiak.pd@gmail.com>
2023-04-04 12:06:42 +0200

    slider

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import './sass/main.scss';
 
 import './js/modals.js';
 import './js/mobile-menu';
+import './js/newsletter-modal';
--- src/js/modals.js
+++ src/js/modals.js
@@ -3,18 +3,23 @@
     heroOpenContactUs: document.querySelector('[hero-open-contact-us]'),
     storeOpenContactUs: document.querySelector('[store-open-contact-us]'),
     closeContactUs: document.querySelector('[close-contact-us]'),
+    openNewsletter: document.querySelector('[open-newsletter]'),
 
     openThankYou: document.querySelector('[open-thank-you'),
     closeThankYou: document.querySelector('[close-thank-you]'),
     continue: document.querySelector('[continue]'),
+    closeNewsletter: document.querySelector('[close-newsletter]'),
 
     contactUsModal: document.querySelector('[contact-us-modal]'),
     thankYouModal: document.querySelector('[thank-you-modal]'),
+    newsletterModal: document.querySelector('[newsletter-modal]'),
   };
 
   refs.heroOpenContactUs.addEventListener('click', toggleContactUsModal);
   refs.storeOpenContactUs.addEventListener('click', toggleContactUsModal);
   refs.closeContactUs.addEventListener('click', toggleContactUsModal);
+  refs.openNewsletter.addEventListener('click', toggleNewsletterModal);
+  refs.closeNewsletter.addEventListener('click', toggleNewsletterModal);
 
   refs.openThankYou.addEventListener('click', () => {
     refs.contactUsModal.classList.add('is-hidden');
@@ -30,5 +35,9 @@
 
   function toggleThankYouModal() {
     refs.thankYouModal.classList.toggle('is-hidden');
+  }
+
+  function toggleNewsletterModal() {
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
